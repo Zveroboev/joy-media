26d1a6e4a0fbd524eb18506195ea219d9a914c4a
Sergey Zveroboev <szveroboev@gmail.com>
2017-09-25 22:50:27 +0300

    Добавил выдвижное меню

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ const PIN = PROGRESS_BAR.querySelector('.video__progress-bar-pin');
 const POPUP = document.querySelector('.popup');
 const POPUP_CLOSE = POPUP.querySelector('.popup__close');
 
+const MENU_TOGGLER = document.querySelector('.main-header__toggler');
+
 // Обработчик события окончания анимации загрузки видео
 const onVideoLoad = () => {
   PIN.classList.remove('video__progress-bar-pin--hidden');
@@ -26,6 +28,8 @@ const onClosePopup = () => {
   POPUP_CLOSE.removeEventListener('click', onClosePopup);
   // Разрешаем скролл
   document.body.style.overflow = 'auto';
+  // Сбрасываем инпут к изначальному состоянию
+  POPUP.querySelector('.popup__input').checked = false;
 };
 
 // Обработчик открытия попапа
@@ -36,10 +40,17 @@ const onOpenPopup = () => {
   POPUP_CLOSE.addEventListener('click', onClosePopup);
   // Запрещаем скролл
   document.body.style.overflow = 'hidden';
+};
 
+// Обработчик открытия бокового меню
+const onMenuClick = () => {
+  document.querySelector('.theme__wrapper').classList.toggle('theme__wrapper--opened');
+  document.querySelector('.main-menu').classList.toggle('main-menu--opened');
 };
 
 // Следим за анимацией загрузки видео
 PRELOAD.addEventListener('animationend', onVideoLoad);
 // Следим за кликом по кнопке "СМОТЕРТЬ"
 SERVICE.addEventListener('click', onOpenPopup);
+// Следим за кнопкой открытия бокового меню
+MENU_TOGGLER.addEventListener('click', onMenuClick);
