9844cc0a69d8794e6e801ba09816499936ade4aa
Sergey Zveroboev <szveroboev@gmail.com>
2017-09-25 20:35:32 +0300

    Закончил попап

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,45 @@
 'use strict';
 
-(function () {
-  const VIDEO_CONTAINER = document.querySelector('.video__wrapper');
-  const PRELOAD_CONTAINER = VIDEO_CONTAINER.querySelector('.video__loading');
-  const PRELOAD = PRELOAD_CONTAINER.querySelector('.video__spinner');
-  const SERVICE = PRELOAD_CONTAINER.querySelector('.video__service');
+const VIDEO_CONTAINER = document.querySelector('.video__wrapper');
+const PRELOAD_CONTAINER = VIDEO_CONTAINER.querySelector('.video__loading');
+const PRELOAD = PRELOAD_CONTAINER.querySelector('.video__spinner');
+const SERVICE = PRELOAD_CONTAINER.querySelector('.video__service');
 
-  const PROGRESS_BAR = document.querySelector('.video__progress-bar-container');
-  const PIN = PROGRESS_BAR.querySelector('.video__progress-bar-pin');
+const PROGRESS_BAR = document.querySelector('.video__progress-bar-container');
+const PIN = PROGRESS_BAR.querySelector('.video__progress-bar-pin');
 
-  // Обработчик события окончания анимации загрузки видео
-  const onVideoLoad = () => {
-    PIN.classList.remove('video__progress-bar-pin--hidden');
-    PRELOAD.classList.add('video__spinner--loaded');
-    SERVICE.classList.add('video__service--loaded');
-  };
+const POPUP = document.querySelector('.popup');
+const POPUP_CLOSE = POPUP.querySelector('.popup__close');
 
-  // Следим за анимацией загрузки видео
-  PRELOAD.addEventListener('animationend', onVideoLoad)
-})();
+// Обработчик события окончания анимации загрузки видео
+const onVideoLoad = () => {
+  PIN.classList.remove('video__progress-bar-pin--hidden');
+  PRELOAD.classList.add('video__spinner--loaded');
+  SERVICE.classList.add('video__service--loaded');
+};
+
+// Обработчик закрытия попапа
+const onClosePopup = () => {
+  // Скрывам попап
+  POPUP.classList.add('popup--hidden');
+  // Убираем слежение за кнокпкой закрытия
+  POPUP_CLOSE.removeEventListener('click', onClosePopup);
+  // Разрешаем скролл
+  document.body.style.overflow = 'auto';
+};
+
+// Обработчик открытия попапа
+const onOpenPopup = () => {
+  // Показываем попап
+  POPUP.classList.remove('popup--hidden');
+  // Вешаем слежение за кнопкой закрытием попапа
+  POPUP_CLOSE.addEventListener('click', onClosePopup);
+  // Запрещаем скролл
+  document.body.style.overflow = 'hidden';
+
+};
+
+// Следим за анимацией загрузки видео
+PRELOAD.addEventListener('animationend', onVideoLoad);
+// Следим за кликом по кнопке "СМОТЕРТЬ"
+SERVICE.addEventListener('click', onOpenPopup);
